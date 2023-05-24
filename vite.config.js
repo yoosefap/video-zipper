6d90a71b817603132a68b7b73f0459f89bbbfbf5
yoosefap <yoosefalipour@gmail.com>
2023-05-24 19:09:22 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import {defineConfig} from 'vite';
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
+import { resolve } from 'path';
 
 export default defineConfig({
+    base: './',
     plugins: [
         crossOriginIsolation()
     ]
