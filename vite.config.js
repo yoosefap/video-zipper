fb60d762e8a4a0a883d9b189d09abfe6a0059315
yoosefap <yoosefalipour@gmail.com>
2024-05-01 19:18:18 +0330

    Update package

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import {defineConfig} from 'vite';
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
-import { resolve } from 'path';
 
 export default defineConfig({
     base: '/video-zipper/',
     plugins: [
-        crossOriginIsolation()
     ]
 });
