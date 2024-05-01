3d480f1daebc7462fe75bb036e1f61be0c5e9823
yoosefap <yoosefalipour@gmail.com>
2024-05-01 18:15:09 +0330

    Add github page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vite';
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
-import { resolve } from 'path';
 
 export default defineConfig({
     base: './',
